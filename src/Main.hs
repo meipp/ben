b5efb0a782edff66acf6a6b645647497ffc936d0
meipp <meipp@users.noreply.github.com>
2023-04-15 08:26:13 +0200

    Add progress bar

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,43 @@
 
 module Main (main) where
 
-import Measurement
+import Measurement (measureCommand)
 import Classification
 import Analysis
 import Tables
 import CmdLine
+import Control.Monad (forM)
 import Data.Aeson (encode)
 import qualified Data.ByteString.Lazy as BL (putStr)
 import System.Directory.PathWalk
+import System.IO
+
+putStrFlush :: String -> IO ()
+putStrFlush s = putStr s >> hFlush stdout
+
+progressBar :: Int -> Int -> Int -> String
+progressBar n w i
+    | n == 0    = "[" ++ replicate w '0' ++ "] " ++ show i ++ "/" ++ show n
+    | n == i    = "[" ++ replicate w '=' ++ "] " ++ show i ++ "/" ++ show n
+    | otherwise = "[" ++ replicate u '=' ++ ">" ++ replicate (w - u - 1) ' ' ++ "] " ++ show i ++ "/" ++ show n
+    where u = if w < n
+                then i `div` (n `div` w)
+                else i * (w `div` n)
+
+forMProgress :: [a] -> (a -> IO b) -> IO ([b])
+forMProgress xs f = do
+    let n = length xs
+    let progress = progressBar n 40
+    ys <- forM (zip [0..] xs) $ \(i, x) -> putStrFlush ("\rProgress: " ++ progress i) >> f x
+    putStrFlush ("\rProgress: " ++ progress n)
+    putStr "\n"
+    return ys
 
 benchmark :: CmdLineArgs -> [(String, String)] -> FilePath -> [(String, String)] -> IO ()
 benchmark args commands files classifiers = do
     fs <- find files
-    measurements <- mapM (uncurry (measureCommand args)) [(c, f) | c <- commands, f <- fs]
+    -- measurements <- mapM (uncurry (measureCommand args)) [(c, f) | c <- commands, f <- fs]
+    measurements <- forMProgress [(c, f) | c <- commands, f <- fs] (uncurry (measureCommand args))
     ms <- mapM (classify classifiers) measurements
     -- BL.putStr (encode ms)
     analyze (map fst classifiers) ms
