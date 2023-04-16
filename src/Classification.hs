2b817e28aeb265293331263839ce557ffd3f30e7
meipp <meipp@users.noreply.github.com>
2023-04-16 18:00:36 +0200

    Refactor Classification.hs

--- src/Classification.hs
+++ src/Classification.hs
@@ -14,12 +14,7 @@ classifyOne cmd input = do
     hClose stdout
     return $ result == ExitSuccess
 
-classifications' :: [(String, String)] -> String -> IO [String]
-classifications' classifiers input = fmap join $ forM classifiers $ \(name, cmd) -> do
+classify :: [(String, String)] -> String -> IO [String]
+classify classifiers input = fmap join $ forM classifiers $ \(name, cmd) -> do
     result <- classifyOne (shell cmd) input
     return $ if result then [name] else []
-
-classify :: [(String, String)] -> String -> IO [String]
-classify classifiers stdout = do
-    classes <- classifications' classifiers stdout
-    return $ classes
